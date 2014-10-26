7675894fb3caf8c2d4e902db4fae5f57a014bbdf
Daniel Abraján C <seorc.d@gmail.com>
2014-10-26 17:23:22 -0600

    Identation correction and corrected output path.

--- assembler/frommd.py
+++ assembler/frommd.py
@@ -14,27 +14,27 @@ class MDPretifier(object):
 
     def go(self):
 
-		env = Environment(loader=PackageLoader(self.project_root, self.template['dir']))
+        env = Environment(loader=PackageLoader(self.project_root, self.template['dir']))
 
-		refs = []
-		for ref in self.map['refs']:
-			refs.append('[{tag}]: {url} "{title}"'.format(**ref))
+        refs = []
+        for ref in self.map['refs']:
+            refs.append('[{tag}]: {url} "{title}"'.format(**ref))
 
-		ref_list = "\n".join(refs)
+        ref_list = "\n".join(refs)
 
-		htmls = []
-		for sect in self.map['sections']:
-			in_file = open(os.path.join(os.path.abspath(self.project_root), sect), 'r')
+        htmls = []
+        for sect in self.map['sections']:
+            in_file = open(os.path.join(os.path.abspath(self.project_root), sect), 'r')
 
-			md = in_file.read().decode('utf8')
-			htmls.append(markdown.markdown(md + "\n" + ref_list))
+            md = in_file.read().decode('utf8')
+            htmls.append(markdown.markdown(md + "\n" + ref_list))
 
-		tpl = env.get_template(self.template['use'])
-		html = "\n".join(htmls)
+        tpl = env.get_template(self.template['use'])
+        html = "\n".join(htmls)
 
-		out = tpl.render(content=html, **self.map)
-		out_file = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.map['output']), 'w')
-		out_file.write(out.encode('utf8'))
+        out = tpl.render(content=html, **self.map)
+        out_file = open(os.path.join(self.project_root, self.map['output']), 'w')
+        out_file.write(out.encode('utf8'))
 
-		in_file.close()
-		out_file.close()
+        in_file.close()
+        out_file.close()
